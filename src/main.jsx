127d58f01f38aaa3be5ee645fda53376b09515b9
dmicaletto <d.micaletto@informatica.aci.it>
2025-12-21 18:28:56 +0100

    Update import statement for index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.jsx' // Assicurati che il tuo file principale si chiami App.jsx dentro src
-import './index.css' // Opzionale, se vuoi stili base
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
