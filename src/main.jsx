45b685a07179b836dcfeef21ad190a71f06cf4c3
dmicaletto <d.micaletto@informatica.aci.it>
2025-12-21 18:09:34 +0100

    Initialize main.jsx with React setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Assicurati che il tuo file principale si chiami App.jsx dentro src
+import './index.css' // Opzionale, se vuoi stili base
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
